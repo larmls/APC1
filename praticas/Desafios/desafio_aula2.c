94d75214ec6087e2ca613a9d2f689692a130ff73
larmls <153823484+larmls@users.noreply.github.com>
2026-09-02 09:15:07 -0300

    Add files via upload

---
+++ praticas/Desafios/desafio_aula2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    const int IDADE = 20;
+    //const double ALTURA = 1.58;
+    //const char NOME = "L aprovado (0/1)";
+
+    printf(IDADE); 
+
+
+
+
+
+    return 0;
+}
+ 
